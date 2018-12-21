e7f39684cfbf40390c776aa32927792c89fc8104
Wallpublisher <salah.awad@wallpublisher.com>
2018-12-21 22:40:19 +0200

    update

--- app.js
+++ app.js
@@ -16,11 +16,20 @@ const
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),
-  path = require('path'),
+  path = require('path'),  
   request = require('request');
 
+  const environment = process.env.NODE_ENV
+  const environmentConfig = config[environment];
+  
+  
+  // as a best practice
+  // all global variables should be referenced via global. syntax
+  // and their names should always begin with g
+  global.gConfig = environmentConfig;
+
 var app = express();
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT  || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
